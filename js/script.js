fbfa6003dbaac7c27481c44121e66610062814a3
Paweł Matysiak <mat.pawel@gmail.com>
2021-03-18 22:09:35 +0100

    Add background & generate title links

--- js/script.js
+++ js/script.js
@@ -5,34 +5,52 @@ const titleClickHandler = function(event){
   const clickedElement = this;
   console.log('Link was clicked!', event);
 
-  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
   console.log('clickedElement:', clickedElement);
-  /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts .active');
+  const activeMovies = document.querySelectorAll('.posts .active');
 
-  for(let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
+  for(let activeMovie of activeMovies) {
+    activeMovie.classList.remove('active');
   }
-  /* [DONE] get 'href' attribute from the clicked link */
   const hrefAttribute = clickedElement.getAttribute('href');
 
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-  const activeArticle = document.querySelector(hrefAttribute);
-  
-  /* [DONE] add class 'active' to the correct article */
-  activeArticle.classList.add('active');
+  const activeMovie = document.querySelector(hrefAttribute);
+
+  activeMovie.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optMovieSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML='';
+  const movies = document.querySelectorAll(optMovieSelector);
+  let html = '';
+
+  for (let movie of movies) {
+
+    const movieId = movie.getAttribute('id');
+    const movieTitle = movie.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + movieId + '"><span>' + movieTitle + '</span></a></li>';
+    html = html + linkHTML;
+    titleList.innerHTML = html;
+  }
+
+  const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+}
+
+generateTitleLinks();
